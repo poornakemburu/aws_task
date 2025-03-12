5798810de189391569bce401d8beda72ca3893b0
Kemburu Poorna Sai Krishna <kemburupoornasai_krishna@epam.com>
2025-03-12 09:58:29 +0530

    task04

---
+++ task04/app/lambdas/sns_handler/index.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+    // TODO implement
+
+    console.log("SNS Event Received:", JSON.stringify(event, null, 2));
+
+        event.Records.forEach(record => {
+            console.log("SNS Message:", record.Sns.Message);
+        });
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
---
+++ task04/app/lambdas/sqs_handler/index.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) => {
+    // TODO implement
+    console.log("SQS Event Received:", JSON.stringify(event, null, 2));
+
+        event.Records.forEach(record => {
+            console.log("SQS Message Body:", record.body);
+        });
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
